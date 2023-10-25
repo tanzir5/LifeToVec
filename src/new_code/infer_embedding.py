72bb5e86f38ed2eabb63d04e80f2adbfcf369f88
tanzir5 <tanzir2430@gmail.com>
2023-10-25 10:09:08 -0400

    infer

--- src/new_code/infer_embedding.py
+++ src/new_code/infer_embedding.py
@@ -4,6 +4,8 @@ import src.transformer
 from src.transformer.models import TransformerEncoder
 from src.new_code.load_data import CustomDataset
 from src.new_code.pretrain import read_hparams_from_file
+from torch.utils.data import DataLoader
+
 import pickle
 
 # hparams_path = 'src/new_code/regular_hparams.txt'
@@ -21,11 +23,25 @@ print(type(dataset))
 print(dataset.data["input_ids"].shape)
 print(dataset.data["original_sequence"].shape)
 dataset.data["input_ids"][:, 0, :] = dataset.data["original_sequence"]
-#dataset['input_ids'][0] = dataset['original_sequence']
-dataset=dataset[:10]
-print(dataset.keys())
-print(dataset["input_ids"].shape)
-print(dataset["original_sequence"].shape)
-print(dataset["sequence_id"].shape)
-#dataset.eval_mode_on()
+batch_size = 10
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
 
+all_outputs = []
+for batch in dataloader:
+    # Move the batch to GPU if available
+    if torch.cuda.is_available():
+        batch = batch.to('cuda')
+
+    # Pass the batch through the model
+    with torch.no_grad():
+        outputs = model(batch)
+
+    # Append the outputs to the list
+    all_outputs.append(outputs.cpu())  # Move back to CPU if necessary
+    print(type(all_outputs[-1]))
+    print(all_outputs[-1])
+    
+# Concatenate the outputs along the batch dimension
+all_outputs = torch.cat(all_outputs, dim=0)
+
+# 'all_outputs' now contains the model's predictions or outputs for the entire dataset
