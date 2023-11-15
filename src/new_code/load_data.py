4e903200290ade7c70df4d5dde64f81cbacf8b13
tanzir5 <tanzir2430@gmail.com>
2023-11-15 00:24:58 -0500

    fixes CustomDataset in load_data

--- src/new_code/load_data.py
+++ src/new_code/load_data.py
@@ -5,6 +5,10 @@ from torch.utils.data import Dataset, DataLoader
 class CustomDataset(Dataset):
     def __init__(self, data):
         self.data = data
+        self.return_index = False
+
+    def set_return_index(self, return_index):
+      self.return_index = return_index
 
     def __len__(self):
         return self.data["input_ids"].shape[0]
@@ -12,8 +16,7 @@ class CustomDataset(Dataset):
         return (self.__class__, (self.data,))
 
     def __getitem__(self, index):
-        return {
-            "sequence_id": self.data["sequence_id"][index],
+        ret_dict = {            
             "original_sequence": self.data["original_sequence"][index],
             "input_ids": self.data["input_ids"][index],
             "padding_mask": self.data["padding_mask"][index],
@@ -21,6 +24,11 @@ class CustomDataset(Dataset):
             "target_pos": self.data["target_pos"][index],
             "target_cls": self.data["target_cls"][index],
         }
+        if self.return_index:
+          ret_dict["sequence_id"] = self.data["sequence_id"][index]
+
+        return ret_dict
+
 '''
 file_path = 'dummy_data.json'
 with open(file_path, "r") as json_file:
